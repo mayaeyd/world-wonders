597ae6a2034bdf0abe7bc8585aa4fb573276fd5c
Maya Eid <mayabaseleid@gmail.com>
2024-11-03 20:25:50 +0200

    Fix masonry layout styling issues

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,34 @@
 // const axios = require('axios');
 
-
-
 axios.get('https://www.world-wonders-api.org/v0/wonders')
     .then(response => {
         const wondersDiv = document.getElementById('wonders-container');
         const result = response.data;
 
         wondersDiv.innerHTML = '';
+
         
         result.forEach(wonder => {
             const wonderDiv = document.createElement('div');
             const image = document.createElement('img');
+            const name = document.createElement('div');
 
             wonderDiv.classList.add('wonder-container');
             image.classList.add('image-container');
+            name.classList.add('wonder-title');
 
             image.src=wonder.links.images[0];
+            image.setAttribute('loading', 'lazy');
+            name.textContent = wonder.name;
 
             wonderDiv.appendChild(image);
+            wonderDiv.appendChild(name);
             wondersDiv.appendChild(wonderDiv);
         });
-        new Masonry(wondersDiv, {
+
+        new Masonry(wondersDiv,{
             itemSelector: '.wonder-container',
-            columnWidth: '.wonder-container',
-            percentPosition: true
+            gutter:4,
         });
         
     })
